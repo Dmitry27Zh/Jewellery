6f055fa3e55e467fbc0248c2c9ccf68b6eab869c
Dmitry <dmitry27zh@gmail.com>
2021-04-21 14:15:57 +0300

    Правки после ревью

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ gulp.task('sprite', function () {
 gulp.task('html', function () {
   return gulp.src('source/*.html')
       .pipe(posthtml([
-        include()
+        include({ encoding: 'utf8' })
       ]))
       .pipe(gulp.dest('build'));
 });
